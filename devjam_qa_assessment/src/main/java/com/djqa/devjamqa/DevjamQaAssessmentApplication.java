b63f6c69a3f15d560a7273d9dcdfd2ba1dcf0b94
Josh Mayfield <josh.mayfield@ruralsource.com>
2018-09-20 16:12:47 -0500

    Initial Commit

---
+++ devjam_qa_assessment/src/main/java/com/djqa/devjamqa/DevjamQaAssessmentApplication.java
@@ -0,0 +1,12 @@
+package com.djqa.devjamqa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevjamQaAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevjamQaAssessmentApplication.class, args);
+	}
+}
